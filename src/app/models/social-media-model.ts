24996098a208a3b081bfe20994f12d3df0875d10
kalananki <hosseinkalanaki@gmail.com>
2023-10-26 13:50:30 +0330

    add link to social media json data

--- src/app/models/social-media-model.ts
+++ src/app/models/social-media-model.ts
@@ -4,13 +4,16 @@ export class SocialMediaModel {
         name: string
         , icon: string
         , address: string
+        ,link: string
     ) {
         this.name = name;
         this.icon = icon;
         this.address = address;
+        this.link = link;
     }
 
     public name: string;
     public icon: string;
     public address: string;
+    public link: string;
 }
